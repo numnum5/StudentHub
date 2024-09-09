4d1175d3b8f87927257e293327255d8ef83edc85
PPham853 <ryan080503@gmail.com>
2024-09-09 13:43:27 +1000

    Change hello-view.fxml to nav-bar.fxml
    .

--- src/main/java/com/example/cab302project/HelloApplication.java
+++ src/main/java/com/example/cab302project/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
     public static final int HEIGHT = 560;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("nav-bar.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
